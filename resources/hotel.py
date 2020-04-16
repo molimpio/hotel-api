824d0ee263446bef716a354361c43d90e8b6df3a
Moisés <moiza.olimpio@gmail.com>
2020-04-16 09:27:20 -0300

    Atualizando hotel

--- resources/hotel.py
+++ resources/hotel.py
@@ -31,35 +31,42 @@ class Hoteis(Resource):
 
 
 class Hotel(Resource):
-    def get(self, hotel_id):
+    argumentos = reqparse.RequestParser()
+    argumentos.add_argument('nome')
+    argumentos.add_argument('estrelas')
+    argumentos.add_argument('cidade')
+    argumentos.add_argument('diaria')
+
+    def find_hotel(hotel_id):
         for hotel in hoteis:
             if hotel['hotel_id'] == hotel_id:
                 return hotel
+        return None
+
+    def get(self, hotel_id):
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            return hotel
         return {'message': 'Hotel não encontrado'}, 404
 
     def post(self, hotel_id):
-        argumentos = reqparse.RequestParser()
-        argumentos.add_argument('nome')
-        argumentos.add_argument('estrelas')
-        argumentos.add_argument('cidade')
-        argumentos.add_argument('diaria')
-
-        # cria dicionario
-        dados = argumentos.parse_args()
-
-        novo_hotel = {
-            'hotel_id': hotel_id,
-            'nome': dados['nome'],
-            'estrelas': dados['estrelas'],
-            'diaria': dados['diaria'],
-            'cidade': dados['cidade'],
-        }
+        dados = Hotel.argumentos.parse_args()
+        novo_hotel = {'hotel_id': hotel_id, **dados}
 
         hoteis.append(novo_hotel)
-        return novo_hotel, 200
+        return novo_hotel, 201
 
     def put(self, hotel_id):
-        pass
+        dados = Hotel.argumentos.parse_args()
+        novo_hotel = {'hotel_id': hotel_id, **dados}
+
+        hotel = Hotel.find_hotel(hotel_id)
+        if hotel:
+            hotel.update(novo_hotel)
+            return novo_hotel, 200
+
+        hoteis.append(novo_hotel)
+        return novo_hotel, 201
 
     def delete(self, hotel_id):
         pass
