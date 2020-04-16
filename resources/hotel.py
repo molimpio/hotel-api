cd75b960899d5da91fe9f64b8225c5ba94fd090e
Moisés <moiza.olimpio@gmail.com>
2020-04-16 08:59:41 -0300

    Buscando hotel pelo ID

--- resources/hotel.py
+++ resources/hotel.py
@@ -28,3 +28,20 @@ hoteis = [
 class Hoteis(Resource):
     def get(self):
         return {'hoteis': hoteis}
+
+
+class Hotel(Resource):
+    def get(self, hotel_id):
+        for hotel in hoteis:
+            if hotel['hotel_id'] == hotel_id:
+                return hotel
+        return {'message': 'Hotel não encontrado'}, 404
+
+    def post(self, hotel_id):
+        pass
+
+    def put(self, hotel_id):
+        pass
+
+    def delete(self, hotel_id):
+        pass
