1d062f627db1b0d9a959535aed1bcd48a388d446
Moisés <moiza.olimpio@gmail.com>
2020-04-17 08:02:42 -0300

    Configurando sqlalchemy

--- app.py
+++ app.py
@@ -3,10 +3,23 @@ from flask_restful import Api
 from resources.hotel import Hoteis, Hotel
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
+
+
+# Antes da primeira request, cria o banco de dados
+@app.before_first_request
+def cria_banco():
+    banco.create_all()
+
 
 api.add_resource(Hoteis, '/hoteis')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
 
-if __name__ == 'main':
+# Usa o import do sql_alchemy dentro do main
+# Só sera chamado caso venha do app.py
+if __name__ == '__main__':
+    from sql_alchemy import banco
+    banco.init_app(app)
     app.run(debug=True)
