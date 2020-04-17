319b6538f3f0322b492b734a5405fa62a6103dbd
Moisés <moiza.olimpio@gmail.com>
2020-04-17 06:52:41 -0300

    Separando classe hotel, refatoração

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource, reqparse
+from models.hotel import HotelModel
 
 hoteis = [
     {
@@ -51,16 +52,17 @@ class Hotel(Resource):
 
     def post(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
-        novo_hotel = {'hotel_id': hotel_id, **dados}
-
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
         hoteis.append(novo_hotel)
         return novo_hotel, 201
 
     def put(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
-        novo_hotel = {'hotel_id': hotel_id, **dados}
-
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
         hotel = Hotel.find_hotel(hotel_id)
+
         if hotel:
             hotel.update(novo_hotel)
             return novo_hotel, 200
