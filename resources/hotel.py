53419f9b863f3ac4bedc6dda94aefdf6b778c418
Moisés <moiza.olimpio@gmail.com>
2020-04-20 06:41:22 -0300

    Implementando put hotel

--- resources/hotel.py
+++ resources/hotel.py
@@ -55,16 +55,17 @@ class Hotel(Resource):
 
     def put(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
-        hotel_objeto = HotelModel(hotel_id, **dados)
-        novo_hotel = hotel_objeto.json()
-        hotel = Hotel.find_hotel(hotel_id)
 
-        if hotel:
-            hotel.update(novo_hotel)
-            return novo_hotel, 200
+        hotel_encontrado = HotelModel.find_hotel(hotel_id)
 
-        hoteis.append(novo_hotel)
-        return novo_hotel, 201
+        if hotel_encontrado:
+            hotel_encontrado.update_hotel(**dados)
+            hotel_encontrado.save_hotel()
+            return hotel_encontrado.json(), 200
+
+        hotel = HotelModel(hotel_id, **dados)
+        hotel.save_hotel()
+        return hotel.json(), 201
 
     def delete(self, hotel_id):
         global hoteis
