26ac75382fec88bb98af2510c55c856d1d080c9a
Moisés <moiza.olimpio@gmail.com>
2020-04-20 06:48:53 -0300

    Implementando get hoteis

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,34 +1,10 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
 
-hoteis = [
-    {
-        'hotel_id': 'alpha',
-        'nome': 'Alpha Hotel',
-        'estrelas': 1.3,
-        'diaria': 120.34,
-        'cidade': 'São Paulo'
-    },
-    {
-        'hotel_id': 'bravo',
-        'nome': 'Bravo Hotel',
-        'estrelas': 4.7,
-        'diaria': 490.34,
-        'cidade': 'Belo Horizonte'
-    },
-    {
-        'hotel_id': 'charlie',
-        'nome': 'Charlie Hotel',
-        'estrelas': 2.3,
-        'diaria': 220.34,
-        'cidade': 'Rio de Janeiro'
-    }
-]
-
 
 class Hoteis(Resource):
     def get(self):
-        return {'hoteis': hoteis}
+        return {'hoteis': [hotel.json() for hotel in HotelModel.query.all()]}
 
 
 class Hotel(Resource):
