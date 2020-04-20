4854505a8c5db8799326c2c9f9f8c143b418cedf
Moisés <moiza.olimpio@gmail.com>
2020-04-20 06:30:51 -0300

    Implementando post hotel

--- resources/hotel.py
+++ resources/hotel.py
@@ -38,12 +38,6 @@ class Hotel(Resource):
     argumentos.add_argument('cidade')
     argumentos.add_argument('diaria')
 
-    def find_hotel(hotel_id):
-        for hotel in hoteis:
-            if hotel['hotel_id'] == hotel_id:
-                return hotel
-        return None
-
     def get(self, hotel_id):
         hotel = Hotel.find_hotel(hotel_id)
         if hotel:
@@ -51,11 +45,13 @@ class Hotel(Resource):
         return {'message': 'Hotel não encontrado'}, 404
 
     def post(self, hotel_id):
+        if HotelModel.find_hotel(hotel_id):
+            return {'message': 'Hotel com id {} já existe'.format(hotel_id)}, 400
+
         dados = Hotel.argumentos.parse_args()
-        hotel_objeto = HotelModel(hotel_id, **dados)
-        novo_hotel = hotel_objeto.json()
-        hoteis.append(novo_hotel)
-        return novo_hotel, 201
+        hotel = HotelModel(hotel_id, **dados)
+        hotel.save_hotel()
+        return hotel.json()
 
     def put(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
